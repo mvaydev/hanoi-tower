7d812e766455ae9d2f097c0bcecdfd20f82b2619
invizex <92627767+invizex@users.noreply.github.com>
2023-09-18 17:59:58 +0800

    Production mode

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
 
     output: {
-        path: path.resolve(__dirname, './dist'),
+        path: path.resolve(__dirname, './'),
         filename: 'main.bundle.js'
     },
 
